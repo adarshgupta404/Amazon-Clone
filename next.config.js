12a7cd0f1eb3de520cb886c345638cf95982dc35
adarshgupta404 <gadarsh747@gmail.com>
2023-08-31 15:15:57 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["m.media-amazon.com", "lh3.googleusercontent.com", "https://m.stripe.network"],
+  },
+  env:{
+    stripe_secret:process.env.STRIPE_SECRET_KEY,
+    stripe_webhook_secret:process.env.STRIPE_WEBHOOK_SECRET
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
